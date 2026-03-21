3d408ed041b31b37e5725ec36fdbfcaeccf9573b
krunpy <maksimglinin17984@gmail.com>
2026-03-21 16:35:02 +0500

    add env example

--- backend/internal/task/model.go
+++ backend/internal/task/model.go
@@ -8,9 +8,8 @@ type Task struct {
 	ID          string `json:"id"`
 	UserID      string	`json:"user_id"`
 	Title       string		`json:"title"`
-	Description string		`json:"description"`
 	Date        time.Time  `json:"date"`
 	Difficulty  string     `json:"difficulty"`// easy / medium / hard — влияет на награду
 	Status      string `json:"status"`// pending / done / failed
-	CompletedAt *time.Time	`json:"completed_at"`
+	CompletedAt time.Time	`json:"completed_at"`
 }
